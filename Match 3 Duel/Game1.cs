90dd30762a1ca05ee522c28854c4b4da8e9b61e6
Romain-Ber <romain.bernier@live.fr>
2023-03-22 18:21:16 +0100

    mechanics

--- Match 3 Duel/Game1.cs
+++ Match 3 Duel/Game1.cs
@@ -7,11 +7,17 @@ namespace Match_3_Duel
 {
     public class Gem
     {
+        public int gemX;
+        public int gemY;
+        public Vector2 gemPos;
         public int gemtype;
         public bool matched;
         private Random rnd;
-        public Gem()
+        public Gem(int pX, int pY)
         {
+            gemX = pX;
+            gemY = pY;
+            gemPos = new Vector2(gemX, gemY);
             matched = false;
             rnd = new Random();
             gemtype = rnd.Next(1, 7);
@@ -38,14 +44,29 @@ namespace Match_3_Duel
         private int gridX;
         private int gridY;
         public Texture2D[] gemTiles = new Texture2D[6];
-        MouseState lastState;
         Gem[,] gemGrid;
-        bool extraMove;
+        private bool extraMove;
+        private MouseState lastState;
+        private int mouseStartX;
+        private int mouseStartY;
+        private int mouseDeltaX;
+        private int mouseDeltaY;
+        private int gemRow;
+        private int gemCol;
+        private bool gemSelected;
+        private string moveGem;
 
+        private bool gridFilled;
+        public SpriteFont arialFont;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -53,10 +74,6 @@ namespace Match_3_Duel
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-            _graphics.IsFullScreen = true;
-            _graphics.ApplyChanges();
 
             tileWidth = 179;
             tileHeight = 170;
@@ -65,6 +82,11 @@ namespace Match_3_Duel
 
             gemGrid = new Gem[7, 5];
             extraMove = false;
+
+            gemSelected = false;
+            moveGem = "";
+
+            gridFilled = false;
 
             base.Initialize();
         }
@@ -75,6 +97,7 @@ namespace Match_3_Duel
 
             // TODO: use this.Content to load your game content here
             Texture2D gemTileset = Content.Load<Texture2D>("gemTileset");
+            arialFont = Content.Load<SpriteFont>("arial");
 
             for (int i = 0; i < 6; i++)
             {
@@ -94,15 +117,21 @@ namespace Match_3_Duel
                 Exit();
 
             // TODO: Add your update logic here
-            do
+            if (gridFilled == false)
             {
                 FillGrid();
-                MatchGems();
-                RemoveMatchedGems();
-                GemFall();
-                ResetGemState();
+                gridFilled = true;
             }
-            while(EmptySpaces());
+            //do
+            //{
+            //    FillGrid();
+            //    MatchGems();
+            //    RemoveMatchedGems();
+            //    GemFall();
+            //    ResetGemState();
+            //}
+            //while(EmptySpaces());
+            MouseClicks();
             
             base.Update(gameTime);
         }
@@ -115,7 +144,7 @@ namespace Match_3_Duel
                 {
                     if (gemGrid[i,j] == null)
                     {
-                        gemGrid[i,j] = new Gem();
+                        gemGrid[i,j] = new Gem(i * tileWidth + gridX, j * tileHeight + gridY);
                     }
                 }
             }
@@ -228,13 +257,134 @@ namespace Match_3_Duel
         protected void MouseClicks()
         {
             MouseState newState = Mouse.GetState();
-            bool bClick = false;
             if (newState.LeftButton == ButtonState.Pressed && lastState.LeftButton == ButtonState.Released)
             {
-                bClick = true;
+                mouseStartX = newState.X;
+                mouseStartY = newState.Y;
+                if (mouseStartX > gridX &&
+                    mouseStartX < GraphicsDevice.Viewport.Width - gridX &&
+                    mouseStartY > gridY &&
+                    mouseStartY < GraphicsDevice.Viewport.Height - gridY)
+                {
+                    gemRow = (int)((mouseStartX - gridX) / tileWidth);
+                    gemCol = (int)((mouseStartY - gridY) / tileHeight);
+                    gemSelected = true;
+                }
+            }
+            if (newState.LeftButton == ButtonState.Pressed && gemSelected == true)
+            {
+                gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY);
+                mouseDeltaX = newState.X;
+                mouseDeltaY = newState.Y;
+                //toward the right
+                if (mouseDeltaX > gemGrid[gemRow, gemCol].gemX &&
+                    mouseDeltaY > gemGrid[gemRow, gemCol].gemY &&
+                    mouseDeltaY < gemGrid[gemRow, gemCol].gemY + tileHeight &&
+                    gemRow < 6)
+                {
+                    if (mouseDeltaX - mouseStartX < tileWidth / 3)
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX + tileWidth, gemGrid[gemRow, gemCol].gemY);
+                        gemGrid[gemRow + 1, gemCol].gemPos = new Vector2(gemGrid[gemRow + 1, gemCol].gemX - tileWidth, gemGrid[gemRow + 1, gemCol].gemY);
+                        moveGem = "RIGHT";
+                    }
+                    else
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY);
+                        gemGrid[gemRow + 1, gemCol].gemPos = new Vector2(gemGrid[gemRow + 1, gemCol].gemX, gemGrid[gemRow + 1, gemCol].gemY);
+                        moveGem = "";
+                    }
+                }
+                //toward the left
+                if (mouseDeltaX < gemGrid[gemRow, gemCol].gemX &&
+                    mouseDeltaY > gemGrid[gemRow, gemCol].gemY &&
+                    mouseDeltaY < gemGrid[gemRow, gemCol].gemY + tileHeight &&
+                    gemRow > 0)
+                {
+                    if (mouseStartX - mouseDeltaX < tileWidth / 3)
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX - tileWidth, gemGrid[gemRow, gemCol].gemY);
+                        gemGrid[gemRow - 1, gemCol].gemPos = new Vector2(gemGrid[gemRow + 1, gemCol].gemX + tileWidth, gemGrid[gemRow + 1, gemCol].gemY);
+                        moveGem = "LEFT";
+                    }
+                    else
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY);
+                        gemGrid[gemRow - 1, gemCol].gemPos = new Vector2(gemGrid[gemRow - 1, gemCol].gemX, gemGrid[gemRow - 1, gemCol].gemY);
+                        moveGem = "";
+                    }
+                }
+                //downward
+                if (mouseDeltaY > gemGrid[gemRow, gemCol].gemY &&
+                    mouseDeltaX > gemGrid[gemRow, gemCol].gemX &&
+                    mouseDeltaX < gemGrid[gemRow, gemCol].gemX + tileWidth &&
+                    gemCol < 4)
+                {
+                    if (mouseDeltaY - mouseStartY < tileHeight / 3)
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY + tileHeight);
+                        gemGrid[gemRow, gemCol + 1].gemPos = new Vector2(gemGrid[gemRow, gemCol + 1].gemX, gemGrid[gemRow, gemCol + 1].gemY - tileHeight);
+                        moveGem = "DOWN";
+                    }
+                    else
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY);
+                        gemGrid[gemRow, gemCol + 1].gemPos = new Vector2(gemGrid[gemRow, gemCol + 1].gemX, gemGrid[gemRow, gemCol + 1].gemY);
+                        moveGem = "";
+                    }
+                }
+                //upward
+                if (mouseDeltaY < gemGrid[gemRow, gemCol].gemY &&
+                    mouseDeltaX > gemGrid[gemRow, gemCol].gemX &&
+                    mouseDeltaX < gemGrid[gemRow, gemCol].gemX + tileWidth &&
+                    gemCol > 0)
+                {
+                    if (mouseStartY - mouseDeltaY < tileHeight / 3)
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY - tileHeight);
+                        gemGrid[gemRow, gemCol - 1].gemPos = new Vector2(gemGrid[gemRow, gemCol - 1].gemX, gemGrid[gemRow, gemCol - 1].gemY + tileHeight);
+                        moveGem = "UP";
+                    }
+                    else
+                    {
+                        gemGrid[gemRow, gemCol].gemPos = new Vector2(gemGrid[gemRow, gemCol].gemX, gemGrid[gemRow, gemCol].gemY);
+                        gemGrid[gemRow, gemCol - 1].gemPos = new Vector2(gemGrid[gemRow, gemCol - 1].gemX, gemGrid[gemRow, gemCol - 1].gemY);
+                        moveGem = "";
+                    }
+                }
+            }
+
+            if (newState.LeftButton == ButtonState.Released && gemSelected == true && moveGem != "")
+            {
+                Gem oGem = gemGrid[gemRow, gemCol];
+                switch (moveGem)
+                {
+                    case "RIGHT":
+                        gemGrid[gemRow, gemCol] = gemGrid[gemRow + 1, gemCol];
+                        gemGrid[gemRow + 1, gemCol] = oGem;
+                        break;
+                    case "LEFT":
+                        gemGrid[gemRow, gemCol] = gemGrid[gemRow - 1, gemCol];
+                        gemGrid[gemRow - 1, gemCol] = oGem;
+                        break;
+                    case "DOWN":
+                        gemGrid[gemRow, gemCol] = gemGrid[gemRow, gemCol + 1];
+                        gemGrid[gemRow, gemCol + 1] = oGem;
+                        break;
+                    case "UP":
+                        gemGrid[gemRow, gemCol] = gemGrid[gemRow, gemCol - 1];
+                        gemGrid[gemRow, gemCol - 1] = oGem;
+                        break;
+                    default:
+                        break;
+                }
+                moveGem = "";
+                gemSelected = false;
+                gemRow = -1;
+                gemCol = -1;
             }
             lastState = newState;
-            bool collideImg = false;
+
         }
 
         protected override void Draw(GameTime gameTime)
@@ -247,12 +397,18 @@ namespace Match_3_Duel
             {
                 for (int j = 0;j < 5; j++)
                 {
-                    //if (gemGrid[i, j] != null)
-                    //{
-                        int type = gemGrid[i, j].gemtype;
-                        Vector2 position = new Vector2(i * tileWidth + gridX, j * tileHeight + gridY);
-                        _spriteBatch.Draw(gemTiles[type - 1], position, Color.White);
-                    //}
+                    int type = gemGrid[i, j].gemtype;
+                    _spriteBatch.Draw(gemTiles[type - 1], gemGrid[i,j].gemPos, Color.White);
+
+                    string coords = gemGrid[i, j].gemX.ToString() + "," + gemGrid[i, j].gemY.ToString();
+                    _spriteBatch.DrawString(arialFont, coords, gemGrid[i, j].gemPos, Color.White);
+                    MouseState mouseState = Mouse.GetState();
+                    string mouseCoords = "current mous coord: " + mouseState.X.ToString() + "," + mouseState.Y.ToString();
+                    _spriteBatch.DrawString(arialFont, mouseCoords, new Vector2(0,0), Color.White);
+                    string gemCoord = "mouse hovering gem [x,y]: " + ((lastState.X - gridX)/tileWidth).ToString() + "," + ((lastState.Y - gridY)/tileHeight).ToString();
+                    _spriteBatch.DrawString(arialFont, gemCoord, new Vector2(0, 25), Color.White);
+                    string gem2Coord = "selected gem [x,y]: " + gemRow.ToString() + "," + gemCol.ToString();
+                    _spriteBatch.DrawString(arialFont, gem2Coord, new Vector2(0,50), Color.White);
                 }
             }
             _spriteBatch.End();
